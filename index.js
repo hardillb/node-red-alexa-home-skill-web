91b363911d7de530231203cd18f8f689ffaa0cb8
Chris Bradford <chrismbradford@outlook.com>
2018-10-02 17:59:50 +0100

    Regression on region

--- index.js
+++ index.js
@@ -324,9 +324,9 @@ app.post('/newuser', function(req,res){
 	var region;
 	log2console("DEBUG", "User country: " + req.body.country.toUpperCase());
 	if (country.statusCode == 200) {
-		//log2console("DEBUG", response);
+		log2console("DEBUG", response);
 		log2console("DEBUG", "User region: " + country.data.region);
-		region = country.body.data.region
+		region = country.data.region
 	}
 	else {
 		log2console("DEBUG", "User region lookup failed.");
