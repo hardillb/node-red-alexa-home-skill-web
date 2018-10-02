5139181e4ab7fde6b8aea128e787a372dca7c425
Chris Bradford <chrismbradford@outlook.com>
2018-10-02 17:46:40 +0100

    Debug++

--- index.js
+++ index.js
@@ -327,7 +327,7 @@ app.post('/newuser', function(req,res){
 	var response = countries.findByCountryCode(req.body.country.toUpperCase());
 	log2console("DEBUG", "User country: " + req.body.country.toUpperCase());
 	if (response.statusCode == 200) {
-		log2console("DEBUG", "User region would be: " + JSON.stringify(response).data.region);
+		log2console("DEBUG", "User region would be: " + JSON.parse(response));
 		log2console("DEBUG", JSON.stringify(response));
 	}
 	else {
