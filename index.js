1f1bff1151e21d2074826da60be28dd01b6d8da8
Chris Bradford <chrismbradford@outlook.com>
2018-10-02 17:31:25 +0100

    Debug ++

--- index.js
+++ index.js
@@ -320,7 +320,10 @@ app.get('/newuser', function(req,res){
 
 app.post('/newuser', function(req,res){
 	// Lookup Region for AWS Lambda/ Web API Skill Interaction
-	console.log("Req:", req.body)
+	console.log("Req:", req)
+	console.log("Req username:", req.body.username)
+	console.log("Req email:", req.body.email)
+	console.log("Req country:", reqbody.country)
 	var response = countries.findByCountryCode(req.body.country.toUpperCase());
 	log2console("DEBUG", "User country:", req.body.country);
 	if (response.statusCode == 200) {
