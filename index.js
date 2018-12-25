1c7089684a0af6d6a15ff54bf9d84dd009803bef
Chris Bradford <chrismbradford@outlook.com>
2018-12-25 22:08:50 +0000

    Debug++

--- index.js
+++ index.js
@@ -381,6 +381,8 @@ app.get('/newuser', function(req,res){
 });
 
 app.post('/newuser', function(req,res){
+	log2console("INFO", "req.body: " + req.body);
+
 	if (req.body.hasOwnProperty(username) && req.body.hasOwnProperty(email) && req.body.hasOwnProperty(country) && req.body.hasOwnProperty(password)) {
 		const country = countries.findByCountryCode(req.body.country.toUpperCase());
 		Promise.all([country]).then(([userCountry]) => {
