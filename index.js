bc7f72cb8fcdda9547a5b707df7a754f4e7554ff
Chris Bradford <chrismbradford@outlook.com>
2018-12-26 19:15:51 +0000

    Debug++

--- index.js
+++ index.js
@@ -1299,6 +1299,7 @@ app.get('/devices',
 		]);
 
 		Promise.all([userDevices, countDevices, countGrants]).then(([devices, countDevs, countUserGrants]) => {
+			log2console("INFO", "Grant count for user: " + user);
 			res.render('pages/devices',{user: req.user, devices: devices, count: countDevs, grants: countUserGrants.countGrants, devs: true});
 		}).catch(err => {
 			res.status(500).json({error: err});
