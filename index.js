9786254113044cbcdf379d6671ba47148837ba53
Chris Bradford <chrismbradford@outlook.com>
2018-12-28 17:29:33 +0000

    Changed logging on redis connect error

--- index.js
+++ index.js
@@ -203,23 +203,23 @@ var client = require('redis').createClient({
         if (options.error && options.error.code === 'ECONNREFUSED') {
             // End reconnecting on a specific error and flush all commands with
             // a individual error
-			log2console("ERROR", "[REDIS] The redis server refused the connection");
+			//log2console("ERROR", "[REDIS] The redis server refused the connection");
 			return new Error('The server refused the connection');
         }
         if (options.total_retry_time > 1000 * 60 * 60) {
             // End reconnecting after a specific timeout and flush all commands
             // with a individual error
-			log2console("ERROR", "[REDIS] Retry time exhausted");
+			//log2console("ERROR", "[REDIS] Retry time exhausted");
 			return new Error('Retry time exhausted');
         }
         if (options.attempt > 10) {
 			// End reconnecting with built in error
-			log2console("ERROR", "[REDIS] Connection retry limit exhausted");
+			//log2console("ERROR", "[REDIS] Connection retry limit exhausted");
             return undefined;
         }
 		// reconnect after
-		log2console("ERROR", "[REDIS] Attempting reconnection after set interval");
-        return Math.min(options.attempt * 100, 3000);
+		//log2console("ERROR", "[REDIS] Attempting reconnection after set interval");
+        return Math.min(options.attempt * 1000, 3000);
    	}
 });
 
