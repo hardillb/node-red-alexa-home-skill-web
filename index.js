5d58f5dc6d2ea63ad5ef4f3a22441686d751ac49
Chris Bradford <chrismbradford@outlook.com>
2019-01-04 19:45:06 +0000

    Change command API logic to handle device that no longer exists in DB

--- index.js
+++ index.js
@@ -1333,9 +1333,12 @@ app.post('/api/v1/command',
 		  }
 		if (enableAnalytics) {visitor.event(params).send()};
 
-
 		Devices.findOne({username:req.user.username, endpointId:req.body.directive.endpoint.endpointId}, function(err, data){
-			if (!err) {
+			if (err) {
+				log2console("ERROR", "[Command API] Unable to lookup device: " + req.body.directive.endpoint.endpointId + " for user: " + req.user.username);
+				res.status(404).send();	
+			}
+			if (data) {
 				// Convert "model" object class to JSON object
 				var deviceJSON = JSON.parse(JSON.stringify(data));
 				var topic = "command/" + req.user.username + "/" + req.body.directive.endpoint.endpointId;
@@ -1389,10 +1392,6 @@ app.post('/api/v1/command',
 					// Command drops into buffer w/ 6000ms timeout (see defined funcitonm above) - ACK comes from N/R flow
 					onGoingCommands[req.body.directive.header.messageId] = command;
 				}
-			}
-			else {
-				log2console("ERROR", "[Command API] Unable to lookup device: " + req.body.directive.endpoint.endpointId + " for user: " + req.user.username);
-				res.status(404).send();
 			}
 		});
 	}
