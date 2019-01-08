cda27d4976dcc511dbbb8b4a2e249d03fb6a7e62
Chris Bradford <chrismbradford@outlook.com>
2019-01-08 14:13:06 +0000

    Added octicons subfolder for svg

--- index.js
+++ index.js
@@ -364,7 +364,7 @@ function requireHTTPS(req, res, next) {
 app.use(requireHTTPS);
 
 app.use('/',express.static('static')); // Static ocntent router
-app.use('/octicons', express.static('node_modules/octicons/build')); // Octicons router
+app.use('/octicons', express.static('node_modules/octicons/build'), express.static('node_modules/octicons/build/svg')); // Octicons router
 
 passport.use(new LocalStrategy(Account.authenticate()));
 
