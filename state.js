659684341452381427b4ff5ffe6f0b1cf8a05879
Chris Bradford <chrismbradford@outlook.com>
2019-01-22 22:36:58 +0000

    Fix bug in requires

--- state.js
+++ state.js
@@ -2,14 +2,14 @@
 const request = require('request');
 // ===============================
 // Schema =======================
-var Account = require('../models/account');
-var oauthModels = require('../models/oauth');
-var Devices = require('../models/devices');
-var Topics = require('../models/topics');
-var LostPassword = require('../models/lostPassword');
+var Account = require('./models/account');
+var oauthModels = require('./models/oauth');
+var Devices = require('./models/devices');
+var Topics = require('./models/topics');
+var LostPassword = require('./models/lostPassword');
 // ===============================
 // Winston Logger ============================
-var logger = require('../config/logger');
+var logger = require('./config/logger');
 var debug = (process.env.ALEXA_DEBUG || false);
 // ===========================================
 // Google Analytics ==========================
