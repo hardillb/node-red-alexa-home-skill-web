3577021dbe73f0fc7474bb925bb6a3bdd5bc471c
Chris Bradford <chrismbradford@outlook.com>
2019-01-27 19:46:16 +0000

    Fix var assignment bug

--- routes/alexa.js
+++ routes/alexa.js
@@ -222,7 +222,7 @@ router.get('/getstate/:dev_id', getStateLimiter,
 			if (data) {
 				const start = async () => {
 					await queryDeviceState(data, function(state) {
-						if (response != undefined) {
+						if (state != undefined) {
 							logger.log('debug', "[State API] Callback returned: " + JSON.stringify(state));
 							res.status(200).json(state);
 						}
