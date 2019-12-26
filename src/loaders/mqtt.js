e17f8c2cf61791415f145c7ee8eb483ed0b219ab
Chris Bradford <chrismbradford@outlook.com>
2019-12-26 11:02:18 +0000

    Add required subscriptions

--- src/loaders/mqtt.js
+++ src/loaders/mqtt.js
@@ -49,8 +49,9 @@ mqttClient.on('reconnect', function(){
 	logger.log('warn', "[MQTT] MQTT reconnect event");
 });
 mqttClient.on('connect', function(){
-	logger.log('info', "[MQTT] MQTT connected, subscribing to 'response/#'")
+	logger.log('info', "[MQTT] MQTT connected, subscribing to 'response/#' and 'state/#")
 	mqttClient.subscribe('response/#');
+	mqttClient.subscribe('state/#');
 });
 ///////////////////////////////////////////////////////////////////////////
 // MQTT Message Handlers
@@ -64,8 +65,151 @@ mqttClient.on('message',function(topic,message){
 	var payload = JSON.parse(message.toString());
 	var commandSource = undefined;
 
+
+
+	// Alexa uses messageId, GHome uses payload.messageId + endpointId as commands can contain multiple devices, this allows for collation
+	var commandWaiting = (ongoingCommands[payload.messageId] || ongoingCommands[payload.messageId + endpointId]);
+
+	if (commandWaiting && commandWaiting.hasOwnProperty('source')){
+		var commandSource = JSON.stringify(commandWaiting.source);
+		commandSource = commandSource.replace(/['"]+/g, '');
+	}
+
+	if (commandWaiting && commandSource == "Google" && topic.startsWith('response/')) {
+		if (payload.success) {
+			// Check if command had >1 target device, if so we need to ensure all commands are successful
+			var arrCommandDevices =  commandWaiting.devices;
+			///////////////////////////////////////////////////////////////////////////
+			// Multi-device command, perform correlation of responses
+			///////////////////////////////////////////////////////////////////////////
+			if (Array.isArray(arrCommandDevices) && arrCommandDevices.length !== 0 ){
+				// Mark this command as acknowledged/ successful as we have a response for it
+				delete ongoingCommands[payload.messageId + endpointId].acknowledged;
+				ongoingCommands[payload.messageId + endpointId].acknowledged = true;
+				//logger.log('debug', "[GHome API] Updated waiting command acknowledged: " + JSON.stringify(ongoingCommands[payload.messageId + endpointId].acknowledged))
+				// Add endpointId to response (if it isn't already there)
+				if (commandWaiting.response.payload.commands[0].ids.includes(endpointId) == false){
+					ongoingCommands[payload.messageId + endpointId].response.payload.commands[0].ids.push(endpointId);
+				}
+				// Check for other commands, and that all are acknowledged
+				var sendResponse = true;
+				for (x = 0; x < arrCommandDevices.length; x++) {
+					var additionalCommand = ongoingCommands[payload.messageId + arrCommandDevices[x]];
+					// Check that endpointId hasn't already been added to response
+					if (additionalCommand) {
+						//logger.log('debug', "[GHome API] Found Additional Command")
+						if (additionalCommand.hasOwnProperty('acknowledged') && commandWaiting.response.payload.commands[0].ids.includes(arrCommandDevices[x]) == false){
+							// Add successful command endpointId to response and delete the additionalCommand that is waiting
+							// Essentially we should get down to a single acknowledged waiting command with deviceIds in response that have successfully executed the command
+							if (additionalCommand.acknowledged == true) {
+								ongoingCommands[payload.messageId + endpointId].response.payload.commands[0].ids.push(arrCommandDevices[x]);
+								// Delete/ clean-up duplicate command response
+								delete ongoingCommands[additionalCommand.requestId + arrCommandDevices[x]];
+								logger.log('debug', "[MQTT] Merged *acknowledged* GHome multi-device command response: " + username +  ", ***updated*** response: " + JSON.stringify(commandWaiting.response));
+							}
+							// This additional command is yet to be acknowledged via MQTT response from Node-RED
+							else {
+								logger.log('debug', "[MQTT] GHome multi-device command *not* acknowledged: " + username +  ", ***un-modified*** response: " + JSON.stringify(commandWaiting.response));
+								sendResponse = false;
+							}
+						}
+						else {
+							sendResponse = false;
+						}
+					}
+				}
+				// All commands in multi-device command have been executed successfully
+				if (sendResponse == true) {
+					logger.log('debug', "[MQTT] Successful GHome multi-device command for user: " + username +  ", response: " + JSON.stringify(commandWaiting.response));
+					try {
+						// Multi-devices this generates an error as res is sent after first device
+						commandWaiting.res.status(200).json(commandWaiting.response);
+						delete ongoingCommands[payload.messageId + endpointId];
+					}
+					catch(e) {
+						logger.log('warn', "[MQTT] GHome send response error: " + e);
+					}
+				}
+
+			}
+			///////////////////////////////////////////////////////////////////////////
+			// Single-device command
+			///////////////////////////////////////////////////////////////////////////
+			else {
+				try {
+					commandWaiting.res.status(200).json(commandWaiting.response);
+					logger.log('debug', "[MQTT] Successful GHome MQTT command for user: " + username +  ", response: " + JSON.stringify(commandWaiting.response));
+					delete ongoingCommands[payload.messageId + endpointId];
+				}
+				catch(e) {
+					logger.log('warn', "[MQTT] GHome send response error: " + e);
+				}
+			}
+		}
+		else {
+			logger.log('debug', "[MQTT] GHome MQTT response message is failure for topic: " + topic);
+			// Google Home failure response
+			if (commandWaiting.hasOwnProperty('source')) {
+				var commandSource = JSON.stringify(commandWaiting.source);
+				commandSource = commandSource.replace(/['"]+/g, '');
+				if (commandSource == "Google") {
+					// Change response to FAILED
+					delete commandWaiting.response.state;
+					commandWaiting.response.status = "FAILED";
+					// Send Response
+					logger.log('warn', "[MQTT] Failed GHome MQTT command for user: " + username + ", response: " + JSON.stringify(commandWaiting.response));
+					try {
+						commandWaiting.res.status(200).json(commandWaiting.response);
+					}
+					catch(e) {
+						logger.log('warn', "[MQTT] Error sending GHome failed command response for user: " + username + ", error: " + e);
+					}
+					delete ongoingCommands[payload.messageId + endpointId];
+				}
+			}
+		}
+	}
+	/// End GHome Response
+	/// Start Alexa Response
+	else if (commandWaiting && commandSource == "Alexa" && topic.startsWith('response/')) {
+		logger.log('info', "[MQTT] Acknowledged Alexa MQTT response message for topic: " + topic);
+		if (payload.success) {
+			//logger.log('debug', "[Alexa API] MQTT response message is success for topic: " + topic);
+			// Alexa success response send to Lambda for full response construction
+			if (commandWaiting.hasOwnProperty('response')) {
+				logger.log('debug', "[MQTT] Successful Alexa MQTT command for user: " + username + ", response: " + JSON.stringify(commandWaiting.response));
+				commandWaiting.res.status(200).json(commandWaiting.response)
+				delete ongoingCommands[payload.messageId];
+			}
+			else {
+				logger.log('debug', "[MQTT] Successful Alexa MQTT command for user: " + username);
+				commandWaiting.res.status(200).send()
+				delete ongoingCommands[payload.messageId];
+			}
+		}
+		else {
+			// Alexa failure response send to Lambda for full response construction
+			if (commandWaiting.hasOwnProperty('source')) {
+				var commandSource = JSON.stringify(commandWaiting.source);
+				commandSource = commandSource.replace(/['"]+/g, '');
+				if (commandSource == "Alexa") {
+					if (commandWaiting.hasOwnProperty('response')) {
+						logger.log('warn', "[MQTT] Failed Alexa MQTT Command for user: " + username + ", response: " + + JSON.stringify(commandWaiting.response));
+						commandWaiting.res.status(503).json(commandWaiting.response)
+						delete ongoingCommands[payload.messageId];
+					}
+					else {
+						logger.log('warn', "[MQTT] Failed Alexa MQTT Command for user: " + username);
+						commandWaiting.res.status(503).send()
+						delete ongoingCommands[payload.messageId];
+					}
+				}
+			}
+		}
+	}
+	/// End Alexa Response
 	/// Start State Handler
-	if (topic.startsWith('state/')){
+	else if (topic.startsWith('state/')){
 		//logger.log('info', "[State API] Acknowledged MQTT state message topic: " + topic);
 		logger.log('info', "[MQTT] Acknowledged MQTT State message for user: " + username + ", message: " + message);
 		// Split topic/ get username and endpointId
@@ -75,151 +219,9 @@ mqttClient.on('message',function(topic,message){
 		setstate(username,endpointId,payload) //arrTopic[1] is username, arrTopic[2] is endpointId
 	}
 	else {
-		// Alexa uses messageId, GHome uses payload.messageId + endpointId as commands can contain multiple devices, this allows for collation
-		var commandWaiting = (ongoingCommands[payload.messageId] || ongoingCommands[payload.messageId + endpointId]);
-
-		if (commandWaiting && commandWaiting.hasOwnProperty('source')){
-			var commandSource = JSON.stringify(commandWaiting.source);
-			commandSource = commandSource.replace(/['"]+/g, '');
-		}
-
-		if (commandWaiting && commandSource == "Google" && topic.startsWith('response/')) {
-			if (payload.success) {
-				// Check if command had >1 target device, if so we need to ensure all commands are successful
-				var arrCommandDevices =  commandWaiting.devices;
-				///////////////////////////////////////////////////////////////////////////
-				// Multi-device command, perform correlation of responses
-				///////////////////////////////////////////////////////////////////////////
-				if (Array.isArray(arrCommandDevices) && arrCommandDevices.length !== 0 ){
-					// Mark this command as acknowledged/ successful as we have a response for it
-					delete ongoingCommands[payload.messageId + endpointId].acknowledged;
-					ongoingCommands[payload.messageId + endpointId].acknowledged = true;
-					//logger.log('debug', "[GHome API] Updated waiting command acknowledged: " + JSON.stringify(ongoingCommands[payload.messageId + endpointId].acknowledged))
-					// Add endpointId to response (if it isn't already there)
-					if (commandWaiting.response.payload.commands[0].ids.includes(endpointId) == false){
-						ongoingCommands[payload.messageId + endpointId].response.payload.commands[0].ids.push(endpointId);
-					}
-					// Check for other commands, and that all are acknowledged
-					var sendResponse = true;
-					for (x = 0; x < arrCommandDevices.length; x++) {
-						var additionalCommand = ongoingCommands[payload.messageId + arrCommandDevices[x]];
-						// Check that endpointId hasn't already been added to response
-						if (additionalCommand) {
-							//logger.log('debug', "[GHome API] Found Additional Command")
-							if (additionalCommand.hasOwnProperty('acknowledged') && commandWaiting.response.payload.commands[0].ids.includes(arrCommandDevices[x]) == false){
-								// Add successful command endpointId to response and delete the additionalCommand that is waiting
-								// Essentially we should get down to a single acknowledged waiting command with deviceIds in response that have successfully executed the command
-								if (additionalCommand.acknowledged == true) {
-									ongoingCommands[payload.messageId + endpointId].response.payload.commands[0].ids.push(arrCommandDevices[x]);
-									// Delete/ clean-up duplicate command response
-									delete ongoingCommands[additionalCommand.requestId + arrCommandDevices[x]];
-									logger.log('debug', "[MQTT] Merged *acknowledged* GHome multi-device command response: " + username +  ", ***updated*** response: " + JSON.stringify(commandWaiting.response));
-								}
-								// This additional command is yet to be acknowledged via MQTT response from Node-RED
-								else {
-									logger.log('debug', "[MQTT] GHome multi-device command *not* acknowledged: " + username +  ", ***un-modified*** response: " + JSON.stringify(commandWaiting.response));
-									sendResponse = false;
-								}
-							}
-							else {
-								sendResponse = false;
-							}
-						}
-					}
-					// All commands in multi-device command have been executed successfully
-					if (sendResponse == true) {
-						logger.log('debug', "[MQTT] Successful GHome multi-device command for user: " + username +  ", response: " + JSON.stringify(commandWaiting.response));
-						try {
-							// Multi-devices this generates an error as res is sent after first device
-							commandWaiting.res.status(200).json(commandWaiting.response);
-							delete ongoingCommands[payload.messageId + endpointId];
-						}
-						catch(e) {
-							logger.log('warn', "[MQTT] GHome send response error: " + e);
-						}
-					}
-
-				}
-				///////////////////////////////////////////////////////////////////////////
-				// Single-device command
-				///////////////////////////////////////////////////////////////////////////
-				else {
-					try {
-						commandWaiting.res.status(200).json(commandWaiting.response);
-						logger.log('debug', "[MQTT] Successful GHome MQTT command for user: " + username +  ", response: " + JSON.stringify(commandWaiting.response));
-						delete ongoingCommands[payload.messageId + endpointId];
-					}
-					catch(e) {
-						logger.log('warn', "[MQTT] GHome send response error: " + e);
-					}
-				}
-			}
-			else {
-				logger.log('debug', "[MQTT] GHome MQTT response message is failure for topic: " + topic);
-				// Google Home failure response
-				if (commandWaiting.hasOwnProperty('source')) {
-					var commandSource = JSON.stringify(commandWaiting.source);
-					commandSource = commandSource.replace(/['"]+/g, '');
-					if (commandSource == "Google") {
-						// Change response to FAILED
-						delete commandWaiting.response.state;
-						commandWaiting.response.status = "FAILED";
-						// Send Response
-						logger.log('warn', "[MQTT] Failed GHome MQTT command for user: " + username + ", response: " + JSON.stringify(commandWaiting.response));
-						try {
-							commandWaiting.res.status(200).json(commandWaiting.response);
-						}
-						catch(e) {
-							logger.log('warn', "[MQTT] Error sending GHome failed command response for user: " + username + ", error: " + e);
-						}
-						delete ongoingCommands[payload.messageId + endpointId];
-					}
-				}
-			}
-		}
-		/// End GHome Response
-		/// Start Alexa Response
-		else if (commandWaiting && commandSource == "Alexa" && topic.startsWith('response/')) {
-			logger.log('info', "[MQTT] Acknowledged Alexa MQTT response message for topic: " + topic);
-			if (payload.success) {
-				//logger.log('debug', "[Alexa API] MQTT response message is success for topic: " + topic);
-				// Alexa success response send to Lambda for full response construction
-				if (commandWaiting.hasOwnProperty('response')) {
-					logger.log('debug', "[MQTT] Successful Alexa MQTT command for user: " + username + ", response: " + JSON.stringify(commandWaiting.response));
-					commandWaiting.res.status(200).json(commandWaiting.response)
-					delete ongoingCommands[payload.messageId];
-				}
-				else {
-					logger.log('debug', "[MQTT] Successful Alexa MQTT command for user: " + username);
-					commandWaiting.res.status(200).send()
-					delete ongoingCommands[payload.messageId];
-				}
-			}
-			else {
-				// Alexa failure response send to Lambda for full response construction
-				if (commandWaiting.hasOwnProperty('source')) {
-					var commandSource = JSON.stringify(commandWaiting.source);
-					commandSource = commandSource.replace(/['"]+/g, '');
-					if (commandSource == "Alexa") {
-						if (commandWaiting.hasOwnProperty('response')) {
-							logger.log('warn', "[MQTT] Failed Alexa MQTT Command for user: " + username + ", response: " + + JSON.stringify(commandWaiting.response));
-							commandWaiting.res.status(503).json(commandWaiting.response)
-							delete ongoingCommands[payload.messageId];
-						}
-						else {
-							logger.log('warn', "[MQTT] Failed Alexa MQTT Command for user: " + username);
-							commandWaiting.res.status(503).send()
-							delete ongoingCommands[payload.messageId];
-						}
-					}
-				}
-			}
-		}
-		/// End Alexa Response
-		else {
-			logger.log('debug', "[MQTT] Unhandled MQTT message event: " + topic + message);
-		}
+		logger.log('debug', "[MQTT] Unhandled MQTT message event: " + topic + message);
 	}
+
 });
 
 ///////////////////////////////////////////////////////////////////////////
